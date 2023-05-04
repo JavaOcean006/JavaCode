77f942f32fdfdcf0b377de0284b6e8176c40ed73
Yenumula Reddy <yenumula.praveen.reddy@gmail.com>
2023-05-04 08:48:59 +0530

    first commit

---
+++ CustomerNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.demo.exceptions;
+
+public class CustomerNotFoundException extends RuntimeException {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
